e17c3038a318df0f4b01a1b6738d685b66d259c9
Induwara Rathnayake <rathnayakeinduwara23@gmail.com>
2025-03-30 15:43:14 +0530

    Add Github Actions

---
+++ src/test_main.py
@@ -0,0 +1,10 @@
+from .main import app  # Now import main.py correctly
+from fastapi.testclient import TestClient
+
+client = TestClient(app)
+
+
+def test_read_root():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Welcome to PHISHGUARD AI!"}
